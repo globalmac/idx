b7131ba7e12811fd583c4650bfbf22787a70a622
Игорь Панков <pankov@ins24.ru>
2025-03-30 00:48:15 +0300

    Init

--- README.md
+++ README.md
@@ -1,2 +1,294 @@
-# idx
-Пилотный проект бинарной встраиваемой поисковой базы данных на Golang для очень быстрого поиска по числовым ключа (B-tree + отображение в память Mmap)
+# IDX
+
+Пилотный проект бинарной встраиваемой поисковой базы данных KEY => VALUE на Golang для очень быстрого поиска по числовым ключа.
+
+Важно: в активной разработке, использовать только для тестов + скоро добавлю больше примеров использования.
+
+### Особенности: 
+
+- Используется B-tree с UINT32 ключём и значениями в виде Slice и Map (см. ниже примеры)
+- Очень высокая скорость работы с минимальным использованием памяти на чтение
+- Mmap (отображение файлов в память)
+- Функциональный сериализатор/десериализатор данных для значений
+- Состоит из writer - создает индекс (файл БД) и reader - читает файл
+- Функции поиска по ID (Find), итерация по всему файлу (GetAll), поиск значения в мапе (Where), выборка по диапазону (Range с 1 по 5, например)
+
+> Вдохновение и общая идея + сериализатор/десериализатор взяты из формата данных MMDB (MaxMind Database) и в частности: https://github.com/maxmind/mmdbwriter (writer) и https://github.com/oschwald/maxminddb-golang (reader) для поиска по IP-адресам
+
+### Зачем и для чего использовать:
+
+- Идеально подходит для формирования поисковой БД с ключами, например в виде числовом представлении строкового значения (аккуратно с коллизиями)
+- Очень удобные типы данных значений с максимальной производительностью на чтение
+- Можно партиционировать файлы БД (например, по 1 млн записей в файл) и объединять их при поиске
+
+### Типы данных значений:
+
+- Map
+- Slice
+- Bytes
+- String
+- Bool
+- Uint16/32/64/128
+- Int32
+- Float32/64
+
+### Минусы:
+
+- За ключами (Uint32) необходимо следить самостоятельно (в случае повтора - данные перезапишутся)
+- Долгий и ресурсоёмкий процесс формирования файлов БД при индексации больших объёмов данных (лучше делить его на небольшие партиции по 1 млн записей)
+- Файлы БД не шифруются, а хешируются алгоритмом Murmur3 для производительности (будьте аккуратны с чувствительными данными)
+
+## Установка:
+
+```
+go get -u github.com/globalmac/idx
+```
+
+## Примеры использования
+
+
+### Создание индекса (файла БД)
+
+```golang
+
+dbFile, err := os.Create("test.db")
+if err != nil {
+    panic(err)
+}
+defer dbFile.Close()
+
+db, err := writer.New(
+    writer.Config{
+        NodeSize: 32, // В зависимости от кол-ва записей: 24 (файл до 50Mb), 28 (файл до 250Mb) / 32 (файл до 5Gb)
+    },
+)
+if err != nil {
+    panic(err)
+}
+
+record := writer.DataMap{
+    "id":     writer.DataUint32(123),
+    "value":  writer.DataString("Привет!"),
+}
+
+err = db.Insert(123123, record)
+if err != nil {
+    panic(err)
+}
+
+```
+
+### Чтение индекса (файла БД)
+
+```go
+
+db, err := reader.Open("test.db")
+if err != nil {
+    panic(err)
+}
+defer db.Close()
+
+var Record struct {
+    ID     uint32 `idx:"id"`
+    Value  string `idx:"value"`
+}
+
+result := db.Find(id)
+
+if result.Exist() {
+    _ = result.Decode(&Record)
+    fmt.Println(Record.ID, Record.Value)
+}
+
+```
+
+### Комбинированный пример
+
+- createIdx - cоздание индекса (файла БД) на основании рандомных данных
+- find - поиск по ID
+
+```go
+
+package main
+
+import (
+	"fmt"
+	"idx/reader"
+	"idx/writer"
+	"log"
+	"math/rand"
+	"net"
+	"os"
+	"runtime"
+	"runtime/debug"
+	"time"
+)
+
+const (
+	lineLength = 10
+	charset    = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+)
+
+var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func randomString(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func randomBool() bool {
+	return seededRand.Intn(2) == 1
+}
+
+func randomIP() string {
+	return fmt.Sprintf("%d.%d.%d.%d/%s",
+		seededRand.Intn(256), // Генерация числа от 0 до 255
+		seededRand.Intn(256),
+		seededRand.Intn(256),
+		seededRand.Intn(256),
+		"24",
+	)
+}
+
+func createIdx() {
+	dbFile, err := os.Create("test.db")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer dbFile.Close()
+
+	db, err := writer.New(
+		writer.Config{
+			NodeSize: 32, // В зависимости от кол-ва записей: 24 (файл до 50Mb), 28 (файл до 250Mb) / 32 (файл до 5Gb)
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 1; i < 100_000; i++ {
+
+		var IP = randomIP()
+		_, network, _ := net.ParseCIDR(IP)
+		rndStr := randomString(lineLength)
+		bigInt := big.Int{}
+		bigInt.SetString("1329227995784915872903807060280344576", 10)
+		uint128 := writer.DataUint128(bigInt)
+
+		record := writer.DataMap{
+			"id":    writer.DataUint32(int32(i)),
+			"name":  writer.DataString("Привет! Строка = " + rndStr),
+			"ip":    writer.DataString(network.IP.String()),
+			"value": writer.DataString(rndStr),
+			"bool":  writer.DataBool(randomBool()),
+			"slice": writer.DataSlice{
+				writer.DataString("строка 1"),
+				writer.DataString("строка 2"),
+				writer.DataString("строка 3"),
+				writer.DataUint64(1),
+				writer.DataUint64(2),
+				writer.DataUint64(3),
+			},
+			"bytes": writer.DataBytes{
+				0x0,
+				0x0,
+				0x0,
+				0x2a,
+			},
+			"double": writer.DataFloat64(42.123456),
+			"float":  writer.DataFloat32(1.1),
+			"int32":  writer.DataInt32(-268435456),
+			"map": writer.DataMap{
+				"item_1": writer.DataMap{
+					"x": writer.DataSlice{
+						writer.DataUint64(0x7),
+						writer.DataUint64(0x8),
+						writer.DataUint64(0x9),
+					},
+					"value": writer.DataString("Счастье"),
+				},
+				"item_2": writer.DataMap{
+					"x": writer.DataSlice{
+						writer.DataUint64(0x7),
+						writer.DataUint64(0x8),
+						writer.DataUint64(0x9),
+					},
+					"value": writer.DataString("Счастье 2"),
+				},
+				"item_3": writer.DataMap{
+					"x": writer.DataSlice{
+						writer.DataUint64(0x7),
+						writer.DataUint64(0x8),
+						writer.DataUint64(0x9),
+					},
+					"value": writer.DataString("Счастье 2"),
+				},
+			},
+			"uint128":     &uint128,
+			"uint16":      writer.DataUint64(0x64),
+			"uint32":      writer.DataUint64(0x10000000),
+			"uint64":      writer.DataUint64(0x1000000000000000),
+			"utf8_string": writer.DataString("unicode! ☯ - ♫"),
+		}
+
+		err = db.Insert(uint32(i), record)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		// Выводим ID
+		fmt.Println(i)
+
+		// Проверяем какие данные будут записаны
+		prefixLen, r := db.Find(uint32(i))
+		fmt.Println("---", prefixLen, r)
+
+	}
+
+	// Сериализуем данные и записываем в файл
+	_, err = db.Serialize(dbFile)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+
+	// Снижение пикового потребления памяти для запуска Garbage Collector
+	os.Setenv("GOGC", "30")
+	debug.SetGCPercent(30)
+
+	// Замеряем начальное использование памяти
+	var startMemStats runtime.MemStats
+	runtime.ReadMemStats(&startMemStats)
+	// Замеряем время начала операции
+	startTime := time.Now()
+
+	//===
+
+	createIdx()
+
+	//===
+
+	// Замеряем время окончания операции
+	elapsedTime := time.Since(startTime)
+	// Замеряем конечное использование памяти
+	var endMemStats runtime.MemStats
+	runtime.ReadMemStats(&endMemStats)
+	// Вычисляем использование памяти
+	memoryUsed := endMemStats.Alloc - startMemStats.Alloc
+
+	// Выводим результаты
+	fmt.Println("-------")
+	fmt.Printf("Время: %v\n", elapsedTime)
+	fmt.Printf("Оперативка: %d байт (%.2f MB)", memoryUsed, float64(memoryUsed)/1024/1024)
+
+}
+```
+
+
+
+
---
+++ reader/read.go
@@ -0,0 +1,602 @@
+package reader
+
+import (
+	"bytes"
+	"errors"
+	w "idx/writer"
+	"io"
+	"iter"
+	"math"
+	"math/big"
+	"os"
+	"reflect"
+	"runtime"
+)
+
+// var sectionSeparatorSize = 16
+const sectionSeparatorSize = 16
+
+const notFound uint = math.MaxUint
+
+type Reader struct {
+	nodeReader     nodeReader
+	buffer         []byte
+	dc             dc
+	Metadata       Metadata
+	nodeOffsetMult uint
+	hasMappedFile  bool
+}
+
+type Metadata struct {
+	BuildEpoch uint `idx:"created_at"`
+	NodeCount  uint `idx:"node_count"`
+}
+
+type K3dt byte
+
+type treeNode struct {
+	id      uint32
+	bit     uint
+	pointer uint
+}
+
+type Result struct {
+	id     uint32
+	err    error
+	dc     dc
+	offset uint
+}
+
+// Open принимает строковый путь к БД и возвращает значение Reader или ошибка.
+// Файл БД открывается с использованием карты памяти
+// на поддерживаемых платформах. На платформах без поддержки отображения данных в память или если
+// попытка сопоставления памяти завершается ошибкой - БД загружается в память.
+// Используйте метод Close для объекта Reader, чтобы освободить ресурсы ОС.
+func Open(file string) (*Reader, error) {
+	mapFile, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer mapFile.Close()
+
+	stats, err := mapFile.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	size64 := stats.Size()
+	// mmapping an empty file returns -EINVAL on Unix platforms,
+	// and ERROR_FILE_INVALID on Windows.
+	if size64 == 0 {
+		return nil, errors.New("file is empty")
+	}
+
+	size := int(size64)
+	// Check for overflow.
+	if int64(size) != size64 {
+		return nil, errors.New("file too large")
+	}
+
+	data, err := mmap(int(mapFile.Fd()), size)
+	if err != nil {
+		if errors.Is(err, errors.ErrUnsupported) {
+			//data, err = openFallback(mapFile, size)
+			data = make([]byte, size)
+			_, err = io.ReadFull(mapFile, data)
+			if err != nil {
+				return nil, err
+			}
+			return OpenRaw(data)
+		}
+		return nil, err
+	}
+
+	reader, err := OpenRaw(data)
+	if err != nil {
+		_ = munmap(data)
+		return nil, err
+	}
+
+	reader.hasMappedFile = true
+	runtime.SetFinalizer(reader, (*Reader).Close)
+	return reader, nil
+}
+
+// OpenRaw берет фрагмент байта, соответствующий файлу БД и возвращает структуру считывателя или ошибку.
+func OpenRaw(buffer []byte) (*Reader, error) {
+	metadataStart := bytes.LastIndex(buffer, w.HeaderMarker)
+
+	if metadataStart == -1 {
+		return nil, newDbError("ошибка при открытии файла БД - некорректный формат файла")
+	}
+
+	metadataStart += len(w.HeaderMarker)
+	metadataDecoder := dc{buffer: buffer[metadataStart:]}
+
+	var metadata Metadata
+
+	rvMetadata := reflect.ValueOf(&metadata)
+	_, err := metadataDecoder.decode(0, rvMetadata, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	searchTreeSize := metadata.NodeCount * (32 / 4)
+	dataSectionStart := searchTreeSize + sectionSeparatorSize
+	dataSectionEnd := uint(metadataStart - len(w.HeaderMarker))
+	if dataSectionStart > dataSectionEnd {
+		return nil, newDbError("файл БД содержит некорректный формат метаданных")
+	}
+	d := dc{
+		buffer: buffer[searchTreeSize+sectionSeparatorSize : metadataStart-len(w.HeaderMarker)],
+	}
+
+	nb := buffer[:searchTreeSize]
+	var nr = nodeReader{buffer: nb}
+
+	reader := &Reader{
+		buffer:         buffer,
+		nodeReader:     nr,
+		dc:             d,
+		Metadata:       metadata,
+		nodeOffsetMult: 32 / 4,
+	}
+
+	return reader, err
+}
+
+// Close освобождает ресурсы ОС
+func (r *Reader) Close() error {
+	var err error
+	if r.hasMappedFile {
+		runtime.SetFinalizer(r, nil)
+		r.hasMappedFile = false
+		err = munmap(r.buffer)
+	}
+	r.buffer = nil
+	return err
+}
+
+// Find возвращает 1 узел
+func (r *Reader) Find(id uint32) Result {
+	if r.buffer == nil {
+		return Result{err: errors.New("БД недоступна для чтения")}
+	}
+
+	// Предварительно вычисленные константы
+	nodeCount := r.Metadata.NodeCount
+	offsetMult := r.nodeOffsetMult
+	nodeReaders := r.nodeReader
+
+	// Обход дерева
+	var node uint
+	var prefixLen uint8
+	for prefixLen = 0; prefixLen < 32 && node < nodeCount; prefixLen++ {
+		// Получаем бит на текущей позиции (0-31)
+		bit := (id >> (31 - prefixLen)) & 1
+
+		offset := node * offsetMult
+		if bit == 0 {
+			node = nodeReaders.readLeft(offset)
+		} else {
+			node = nodeReaders.readRight(offset)
+		}
+	}
+
+	// Обработка результатов
+	if node == nodeCount {
+		// Запись не найдена
+		return Result{
+			id:     id,
+			offset: notFound,
+		}
+	} else if node > nodeCount {
+		// Нашли указатель на данные
+		offset, err := r.resolveDataPointer(node)
+		return Result{
+			dc:     r.dc,
+			id:     id,
+			offset: uint(offset),
+			err:    err,
+		}
+	}
+
+	// Некорректная структура базы данных
+	return Result{
+		err: newDbError("Некорректная структура узла БД"),
+	}
+}
+
+// GetAll возвращает итератор по всем узлам
+func (r *Reader) GetAll() iter.Seq[Result] {
+	return r.Scan(0, 0)
+}
+
+func (r *Reader) Where(fieldName string, fieldValue interface{}, yield func(Result) bool) {
+	if r.buffer == nil {
+		return
+	}
+
+	var expectedKind reflect.Kind
+	switch fieldValue.(type) {
+	case string:
+		expectedKind = reflect.String
+	case uint32:
+		expectedKind = reflect.Uint32
+	case int:
+		expectedKind = reflect.Int
+	case bool:
+		expectedKind = reflect.Bool
+	case float64:
+		expectedKind = reflect.Float64
+	default:
+		return
+	}
+
+	var stack [64]struct {
+		node   uint
+		nodeID uint32
+		bit    uint8
+	}
+	stackSize := 1
+	stack[0] = struct {
+		node   uint
+		nodeID uint32
+		bit    uint8
+	}{node: 0, nodeID: 0, bit: 0}
+
+	nodeCount := r.Metadata.NodeCount
+	offsetMult := r.nodeOffsetMult
+
+	for stackSize > 0 {
+		stackSize--
+		item := stack[stackSize]
+		node, nodeID, bit := item.node, item.nodeID, item.bit
+
+		if node >= nodeCount {
+			if node > nodeCount {
+				dataOffset, err := r.resolveDataPointer(node)
+				if err != nil {
+					continue
+				}
+
+				// Исправление: преобразуем uintptr в uint для работы с декодером
+				offset := uint(dataOffset)
+				typeNum, size, newOffset, err := r.dc.decodeCtrlData(offset)
+				if err != nil {
+					continue
+				}
+
+				if typeNum == w.TypeMap {
+					found := false
+					currentOffset := newOffset
+					for i := uint(0); i < size; i++ {
+						key, nextOffset, err := r.dc.decodeKey(currentOffset)
+						if err != nil {
+							break
+						}
+
+						if string(key) == fieldName {
+							fieldOffset := nextOffset
+							match := false
+
+							switch expectedKind {
+							case reflect.String:
+								var val string
+								_, err := r.dc.decode(fieldOffset, reflect.ValueOf(&val).Elem(), 0)
+								match = err == nil && val == fieldValue.(string)
+							case reflect.Uint32:
+								var val uint32
+								_, err := r.dc.decode(fieldOffset, reflect.ValueOf(&val).Elem(), 0)
+								match = err == nil && val == fieldValue.(uint32)
+							}
+
+							if match {
+								if !yield(Result{
+									dc:     r.dc,
+									id:     nodeID,
+									offset: offset,
+									//prefixLen: bit,
+								}) {
+									return
+								}
+							}
+							found = true
+							break
+						}
+
+						// Исправление: работаем с uint для nextValueOffset
+						currentOffset, err = r.dc.nextValueOffset(nextOffset, 1)
+						if err != nil {
+							break
+						}
+					}
+					if found {
+						continue
+					}
+				}
+			}
+			continue
+		}
+
+		if stackSize+2 < len(stack) {
+			offset := node * offsetMult
+			leftPointer := r.nodeReader.readLeft(offset)
+			rightPointer := r.nodeReader.readRight(offset)
+
+			rightID := nodeID | (1 << (31 - bit))
+			nextBit := bit + 1
+
+			stack[stackSize] = struct {
+				node   uint
+				nodeID uint32
+				bit    uint8
+			}{node: rightPointer, nodeID: rightID, bit: nextBit}
+			stackSize++
+
+			stack[stackSize] = struct {
+				node   uint
+				nodeID uint32
+				bit    uint8
+			}{node: leftPointer, nodeID: nodeID, bit: nextBit}
+			stackSize++
+		}
+	}
+}
+
+func (r *Reader) Scan(id uint32, prefixLen uint8) iter.Seq[Result] {
+	return func(yield func(Result) bool) {
+
+		stopBit := int(prefixLen)
+		if stopBit > 32 {
+			stopBit = 32
+		}
+
+		pointer, bit := r.traverseTree(id, 0, stopBit)
+
+		mask := uint32(0xFFFFFFFF) << (32 - bit)
+		network := id & mask
+
+		nodes := []treeNode{{
+			id:      network,
+			bit:     uint(bit),
+			pointer: pointer,
+		}}
+
+		for len(nodes) > 0 {
+			node := nodes[len(nodes)-1]
+			nodes = nodes[:len(nodes)-1]
+
+			for {
+				if node.pointer == r.Metadata.NodeCount {
+					break
+				}
+
+				if node.pointer > r.Metadata.NodeCount {
+					offset, err := r.resolveDataPointer(node.pointer)
+					ok := yield(Result{
+						dc:     r.dc,
+						id:     node.id,
+						offset: uint(offset),
+						err:    err,
+					})
+					if !ok {
+						return
+					}
+					break
+				}
+
+				// Создаем правую ветку с установленным битом
+				idRight := node.id | (1 << (31 - node.bit))
+
+				offset := node.pointer * r.nodeOffsetMult
+				rightPointer := r.nodeReader.readRight(offset)
+
+				node.bit++
+				nodes = append(nodes, treeNode{
+					pointer: rightPointer,
+					id:      idRight,
+					bit:     node.bit,
+				})
+
+				node.pointer = r.nodeReader.readLeft(offset)
+			}
+		}
+	}
+}
+
+func (r *Reader) GetRange(start, end uint32) iter.Seq[Result] {
+	return func(yield func(Result) bool) {
+		if start > end {
+			return
+		}
+
+		// Предварительно вычисленные константы
+		nodeCount := r.Metadata.NodeCount
+		offsetMult := r.nodeOffsetMult
+		nr := r.nodeReader
+
+		// Фиксированный стек (достаточно для 32 уровней глубины)
+		var stack [64]struct {
+			node   uint
+			nodeID uint32
+			bit    uint8
+		}
+		stackSize := 1
+		stack[0] = struct {
+			node   uint
+			nodeID uint32
+			bit    uint8
+		}{node: 0, nodeID: 0, bit: 0}
+
+		for stackSize > 0 {
+			// Извлекаем текущий элемент
+			stackSize--
+			item := stack[stackSize]
+			node, nodeID, bit := item.node, item.nodeID, item.bit
+
+			// Вычисляем границы текущей сети
+			mask := ^uint32(0) << (32 - bit)
+			netStart := nodeID & mask
+			netEnd := netStart | ^mask
+
+			// Проверяем пересечение с нашим диапазоном
+			if netStart > end || netEnd < start {
+				continue
+			}
+
+			if node >= nodeCount {
+				if node != nodeCount {
+					offset, err := r.resolveDataPointer(node)
+					if !yield(Result{
+						dc:     r.dc,
+						id:     netStart,
+						offset: uint(offset),
+						//prefixLen: bit,
+						err: err,
+					}) {
+						return
+					}
+				}
+				continue
+			}
+
+			// Вычисляем адрес для правой ветки
+			rightBit := bit + 1
+			rightID := netStart | (1 << (31 - bit))
+
+			// Читаем указатели на потомков
+			offset := node * offsetMult
+			leftPointer := nr.readLeft(offset)
+			rightPointer := nr.readRight(offset)
+
+			// Добавляем в стек (сначала правую ветку, потом левую)
+			if rightID <= end {
+				if stackSize < len(stack) {
+					stack[stackSize] = struct {
+						node   uint
+						nodeID uint32
+						bit    uint8
+					}{node: rightPointer, nodeID: rightID, bit: rightBit}
+					stackSize++
+				}
+			}
+
+			if netStart <= end {
+				if stackSize < len(stack) {
+					stack[stackSize] = struct {
+						node   uint
+						nodeID uint32
+						bit    uint8
+					}{node: leftPointer, nodeID: netStart, bit: rightBit}
+					stackSize++
+				}
+			}
+		}
+	}
+}
+
+func (r *Reader) traverseTree(id uint32, node uint, stopBit int) (uint, int) {
+	nodeCount := r.Metadata.NodeCount
+
+	for i := 0; i < stopBit && node < nodeCount; i++ {
+		// Получаем бит на позиции i (0-31)
+		bit := (id >> (31 - i)) & 1
+
+		offset := node * r.nodeOffsetMult
+		if bit == 0 {
+			node = r.nodeReader.readLeft(offset)
+		} else {
+			node = r.nodeReader.readRight(offset)
+		}
+	}
+
+	return node, stopBit
+}
+
+func (r *Reader) resolveDataPointer(pointer uint) (uintptr, error) {
+	resolved := uintptr(pointer - r.Metadata.NodeCount - sectionSeparatorSize)
+
+	if resolved >= uintptr(len(r.buffer)) {
+		return 0, newDbError("нарушена структура поискового дерева в БД")
+	}
+	return resolved, nil
+}
+
+func (r Result) Decode(v any) error {
+	if r.err != nil {
+		return r.err
+	}
+	if r.offset == notFound {
+		return nil
+	}
+	rv := reflect.ValueOf(v)
+	if rv.Kind() != reflect.Ptr || rv.IsNil() {
+		return errors.New("result param must be a pointer")
+	}
+
+	if dser, ok := v.(deserializer); ok {
+		_, err := r.dc.decodeToDeserializer(r.offset, dser, 0, false)
+		return err
+	}
+
+	_, err := r.dc.decode(r.offset, rv, 0)
+	return err
+}
+
+func (r Result) DecodePath(v any, path ...any) error {
+	if r.err != nil {
+		return r.err
+	}
+	if r.offset == notFound {
+		return nil
+	}
+	rv := reflect.ValueOf(v)
+	if rv.Kind() != reflect.Ptr || rv.IsNil() {
+		return errors.New("result param must be a pointer")
+	}
+	return r.dc.decodePath(r.offset, path, rv)
+}
+
+func (r Result) Err() error {
+	return r.err
+}
+
+func (r Result) Exist() bool {
+	return r.err == nil && r.offset != notFound
+}
+
+type deserializer interface {
+	ShouldSkip(offset uintptr) (bool, error)
+	StartSlice(size uint) error
+	StartMap(size uint) error
+	End() error
+	String(string) error
+	Float64(float64) error
+	Bytes([]byte) error
+	Uint16(uint16) error
+	Uint32(uint32) error
+	Int32(int32) error
+	Uint64(uint64) error
+	Uint128(*big.Int) error
+	Bool(bool) error
+	Float32(float32) error
+}
+
+type nodeReader struct {
+	buffer []byte
+}
+
+func (n nodeReader) readLeft(nodeNumber uint) uint {
+	return (uint(n.buffer[nodeNumber]) << 24) |
+		(uint(n.buffer[nodeNumber+1]) << 16) |
+		(uint(n.buffer[nodeNumber+2]) << 8) |
+		uint(n.buffer[nodeNumber+3])
+}
+
+func (n nodeReader) readRight(nodeNumber uint) uint {
+	return (uint(n.buffer[nodeNumber+4]) << 24) |
+		(uint(n.buffer[nodeNumber+5]) << 16) |
+		(uint(n.buffer[nodeNumber+6]) << 8) |
+		uint(n.buffer[nodeNumber+7])
+}
